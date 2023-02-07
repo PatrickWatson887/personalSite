1f51c22bb880719aed67ad4e4f7b5d9b671bde78
PatrickWatson887 <patrick.watson887@gmail.com>
2023-02-07 01:58:27 +0000

    updated experience

--- src/components/skillsGrid/index.tsx
+++ src/components/skillsGrid/index.tsx
@@ -2,12 +2,12 @@ import SkillsCard from "../skillsCard";
 
 const SkillsGrid: React.FC = () => {
   const skills = [
-    {title: 'React Native', description: '6 months experience building an app for CST', src: '/react-native.png'},
-    {title: 'Typescript', description: '1 year experience, used in every project since discovery! Love it', src: '/typescript.png'},
-    {title: 'NextJs', description: '1 year experience, was introduced through Better Climbing and continued to expand my knowledge in CST', src: '/nextjs.png'},
+    {title: 'React Native', description: '1 year experience building an app for CST and updating a Lightspeed app', src: '/react-native.png'},
+    {title: 'Typescript', description: '2 year experience, used in every project since discovery! Love it', src: '/typescript.png'},
+    {title: 'NextJs', description: '2 year experience, was introduced through Better Climbing and continued to expand my knowledge in CST', src: '/nextjs.png'},
     {title: 'AWS', description: '4 years experience hosting websites on EC2 or using lambdas/dynamoDb', src: '/aws.png'},
     {title: 'Docker', description: '2 years experience using it in Kainos but only got to grips setting it up since CST', src: '/docker.png'},
-    {title: 'Tailwind', description: '1 year experience, used on both Better Climbing and CST', src: '/tailwind.png'},
+    {title: 'Tailwind', description: '2 year experience, used on both Better Climbing and CST', src: '/tailwind.png'},
   ]
 
   return (
